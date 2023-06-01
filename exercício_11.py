4a4ffb7732d1e2425f781c5b4459f75d1a739f49
Vhs4 <vhszera44444@gmail.com>
2023-06-01 19:09:12 -0300

    [NEW] Novos aprendizados.

---
+++ exercício_11.py
@@ -0,0 +1,23 @@
+def criar_cesta():
+    cesta = []
+    return cesta
+
+
+def adicionar(cesta, fruta):
+    cesta.append(fruta)
+
+
+def remover(cesta, fruta):
+    cesta.remove(fruta)
+
+
+def listar():
+    list.cesta
+    if len(listar) == 5:
+        print(f"O total de frutas é {listar} e a fruta está cheia.")
+    elif len(listar) == 0:
+        print(f"A cesta está vazia.")
+    elif len(listar) > 0 < 5:
+        print(f"O total de frutas é{listar}.")
+    else:
+        print(f"O total de frutas é {listar}")
